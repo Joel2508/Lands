858fb5a87d29244d1012c926cb67237f5917ab28
Joel2508 <joelarias2508@gmail.com>
2020-06-25 19:32:57 +0200

    Modification tabbed page

--- Lands/Lands/ViewModels/LandTabbedPageViewModel.cs
+++ Lands/Lands/ViewModels/LandTabbedPageViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using Common.Helpers;
 using Common.Models;
+using Lands.Views;
 using Newtonsoft.Json;
 using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
+using Prism.Xaml;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,9 +16,19 @@ namespace Lands.ViewModels
     {
         private readonly INavigationService _navigationService;
         private Land _land;
+
+        private DelegateCommand _locationCommand;
         public LandTabbedPageViewModel(INavigationService navigationService) : base(navigationService)
         {
             _navigationService = navigationService;
+            _land = JsonConvert.DeserializeObject<Land>(Settings.Land);
+        }
+
+        public DelegateCommand LocationCommand => _locationCommand ?? (_locationCommand = new DelegateCommand(LocationAsync));
+        private async void LocationAsync()
+        {
+            var parameter = new NavigationParameters { { "land", _land } };
+            await _navigationService.NavigateAsync(nameof(LocationPage), parameter);
         }
 
         public override void OnNavigatedTo(INavigationParameters parameters)
@@ -25,8 +37,7 @@ namespace Lands.ViewModels
             base.OnNavigatedTo(parameters);
 
             if (parameters.ContainsKey("Land"))
-            {
-                _land = parameters.GetValue<Land>("Land");
+            {                
                 Settings.Land = JsonConvert.SerializeObject(_land);
                 Title = _land.Name;
             }
--- Lands/Lands/ViewModels/LanguajesPageViewModel.cs
+++ Lands/Lands/ViewModels/LanguajesPageViewModel.cs
@@ -1,16 +1,31 @@
-﻿using Prism.Commands;
+﻿using Common.Helpers;
+using Common.Models;
+using Lands.Helpers;
+using Newtonsoft.Json;
+using Prism.Commands;
 using Prism.Mvvm;
+using Prism.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace Lands.ViewModels
 {
-    public class LanguajesPageViewModel : BindableBase
+    public class LanguajesPageViewModel : ViewModelBase
     {
-        public LanguajesPageViewModel()
+        private ObservableCollection<Language> languages;
+        public Land Land { get; set; }
+        public ObservableCollection<Language> Languages
         {
-
+            set => SetProperty(ref languages, value);
+            get => languages;
+        }
+        public LanguajesPageViewModel(INavigationService navigationService):base(navigationService)
+        {
+            Title = Helpers.Languages.MyLanguages;
+            Land = JsonConvert.DeserializeObject<Land>(Settings.Land);
+            Languages = new ObservableCollection<Language>(Land.Languages);
         }
     }
 }
