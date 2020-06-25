d132d8fb7e6e2afcd3e59a988d2a7562895fe221
Joel2508 <joelarias2508@gmail.com>
2020-06-25 20:46:07 +0200

    Function location page

--- Lands/Lands/ViewModels/LocationPageViewModel.cs
+++ Lands/Lands/ViewModels/LocationPageViewModel.cs
@@ -1,16 +1,23 @@
-﻿using Prism.Commands;
+﻿using Common.Helpers;
+using Common.Models;
+using Lands.Helpers;
+using Newtonsoft.Json;
+using Prism.Commands;
 using Prism.Mvvm;
+using Prism.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace Lands.ViewModels
 {
-    public class LocationPageViewModel : BindableBase
+    public class LocationPageViewModel : ViewModelBase
     {
-        public LocationPageViewModel()
+        public Land Land { get; set; }
+        public LocationPageViewModel(INavigationService navigationService): base(navigationService)
         {
-
+            Land = JsonConvert.DeserializeObject<Land>(Settings.Land);
+            Title = $"{Languages.Country}: {Land.Name}";
         }
     }
 }
